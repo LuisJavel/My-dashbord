e4762f599009da48752ebf3e4e97366790a0f6ee
LMsqz <luisfmasaquiza@hotmail.com>
2024-01-03 17:37:00 +0100

    correcion

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -27,10 +27,10 @@ export const Sidebar = () => {
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
           <IoLogoReact className="mr-2" />
-          <span>Dash</span>
+          <span>MSQZ</span>
           <span className="text-blue-500">8</span>.
         </h1>
-        <p className="text-slate-500 text-sm">Mneja tus actividades</p>
+        <p className="text-slate-500 text-sm">Maneja tus actividades</p>
       </div>
       <div id="profile" className="px-6 py-10">
         <p className="text-slate-500">Welcome back,</p>
